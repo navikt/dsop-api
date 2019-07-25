445f076bac9fb671206526857fe6c2ba17e335ec
Øyvind Hagen <oyvind.hagen@nav.no>
2019-07-25 11:01:17 +0200

    Lagt isAlive bak internal.

--- src/main/kotlin/no/nav/sbl/dsop/api/platform/Health.kt
+++ src/main/kotlin/no/nav/sbl/dsop/api/platform/Health.kt
@@ -6,6 +6,7 @@ import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 
+
 fun Routing.health(
         ready: () -> Boolean = { true },
         alive: () -> Boolean = { true }
@@ -13,11 +14,11 @@ fun Routing.health(
 
     fun statusFor(b: () -> Boolean) = b().let{ if(it) HttpStatusCode.OK else HttpStatusCode.InternalServerError}
 
-    get("isReady") {
+    get("internal/isReady") {
         statusFor(ready).let { call.respondText("Ready: $it", status = it) }
     }
 
-    get("isAlive") {
+    get("internal/isAlive") {
         statusFor(alive).let { call.respondText("Alive: $it", status = it) }
     }
 
