6c513ee001f12f24aa08d6da2b95df81f2738536
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-02-04 09:04:39 +0100

    Lagt inn logging

--- src/main/kotlin/no/nav/sbl/dsop/oppslag/dsop/DsopCall.kt
+++ src/main/kotlin/no/nav/sbl/dsop/oppslag/dsop/DsopCall.kt
@@ -10,6 +10,7 @@ import io.ktor.client.statement.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import mu.KotlinLogging
 import no.nav.sbl.dsop.api.Environment
 import no.nav.sbl.dsop.api.KODEVERK_TEMA_CACHE
 import no.nav.sbl.dsop.api.OIDC_COOKIE_NAME
@@ -21,46 +22,53 @@ import kotlin.collections.List
 import kotlin.collections.map
 import kotlin.collections.set
 
+private val logger = KotlinLogging.logger {}
+
 fun Route.dsop(env: Environment) {
     get("get") {
-        val selvbetjeningIdtoken = call.request.cookies[OIDC_COOKIE_NAME]
-        val authorization =
-            if (env.isMockedEnvironment()) ""
-            else if (!selvbetjeningIdtoken.isNullOrEmpty()) "Bearer ".plus(selvbetjeningIdtoken)
-            else call.request.header("Authorization")
-                ?: throw IllegalArgumentException("Kunne ikke hente ut brukers OIDC-token.")
-        val dsopClient = HttpClient {
-            defaultRequest {
-                header(env.apiKeyUsername, env.dsopApiSporingsloggLesloggerApiKeyPassword)
-                header("Authorization", authorization)
+        try {
+            val selvbetjeningIdtoken = call.request.cookies[OIDC_COOKIE_NAME]
+            val authorization =
+                if (env.isMockedEnvironment()) ""
+                else if (!selvbetjeningIdtoken.isNullOrEmpty()) "Bearer ".plus(selvbetjeningIdtoken)
+                else call.request.header("Authorization")
+                    ?: throw IllegalArgumentException("Kunne ikke hente ut brukers OIDC-token.")
+            val dsopClient = HttpClient {
+                defaultRequest {
+                    header(env.apiKeyUsername, env.dsopApiSporingsloggLesloggerApiKeyPassword)
+                    header("Authorization", authorization)
+                }
+                install(JsonFeature)
             }
-            install(JsonFeature)
+
+            val dsopResult: HttpResponse = dsopClient.request(env.sporingloggLesloggerUrl)
+            val sporingslogg2 = dsopResult.receive<List<Sporingslogg>>()
+
+            val orgnavnCache = HashMap<String, String>()
+            call.respond(
+                sporingslogg2.map {
+                    if (orgnavnCache[it.mottaker] == null) {
+                        orgnavnCache[it.mottaker] =
+                            getOrganisasjonsnavn(authorization = authorization, orgnr = it.mottaker, environment = env)
+                    }
+                    if (KODEVERK_TEMA_CACHE[it.tema] == null) {
+                        KODEVERK_TEMA_CACHE[it.tema] =
+                            getKodeverk(authorization = authorization, kode = it.tema, environment = env)
+                    }
+
+                    Sporingslogg(
+                        tema = KODEVERK_TEMA_CACHE[it.tema] ?: it.tema,
+                        uthentingsTidspunkt = it.uthentingsTidspunkt,
+                        mottaker = it.mottaker,
+                        mottakernavn = orgnavnCache[it.mottaker],
+                        leverteData = it.leverteData,
+                        samtykkeToken = it.samtykkeToken
+                    )
+                }
+            )
+        } catch (e: Exception) {
+            logger.error("Noe gikk galt i DsopCall", e)
         }
 
-        val dsopResult: HttpResponse = dsopClient.request(env.sporingloggLesloggerUrl)
-        val sporingslogg2 = dsopResult.receive<List<Sporingslogg>>()
-
-        val orgnavnCache = HashMap<String, String>()
-        call.respond(
-            sporingslogg2.map {
-                if (orgnavnCache[it.mottaker] == null) {
-                    orgnavnCache[it.mottaker] =
-                        getOrganisasjonsnavn(authorization = authorization, orgnr = it.mottaker, environment = env)
-                }
-                if (KODEVERK_TEMA_CACHE[it.tema] == null) {
-                    KODEVERK_TEMA_CACHE[it.tema] =
-                        getKodeverk(authorization = authorization, kode = it.tema, environment = env)
-                }
-
-                Sporingslogg(
-                    tema = KODEVERK_TEMA_CACHE[it.tema] ?: it.tema,
-                    uthentingsTidspunkt = it.uthentingsTidspunkt,
-                    mottaker = it.mottaker,
-                    mottakernavn = orgnavnCache[it.mottaker],
-                    leverteData = it.leverteData,
-                    samtykkeToken = it.samtykkeToken
-                )
-            }
-        )
     }
 }
